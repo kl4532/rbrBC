502b2d9381dee9a7009448ea3d848b8a03a0ccd8
kl4532 <kornellukasiak@gmail.com>
2020-04-05 11:24:18 +0200

    navigation test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,76 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { version } from '../../package.json';
+import {Location} from "@angular/common";
+import {fakeAsync, tick} from '@angular/core/testing';
+import {Router} from "@angular/router";
+
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
+import { GeneralComponent } from './general/general.component';
+import { StageComponent } from './stage/stage.component';
+import { routes } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let app;
+  let location: Location;
+  let router: Router;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes(routes)
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        SettingsComponent,
+        StageComponent,
+        GeneralComponent
       ],
     }).compileComponents();
   }));
 
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+
+    router = TestBed.get(Router);
+    router.initialNavigation();
+    location = TestBed.get(Location);
+
+    fixture.detectChanges();
+
+  })
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('should display actual version', () => {
+    expect(app.version).toEqual(version);
+  }) 
+
+  // fit('should navigate to settings on init', fakeAsync(() => { 
+  //   // router.navigate(['general']);
+  //   tick(1000); 
+  //   expect(location.path()).toBe('/settings');
+  // }));
+
+  it('navigate to <path> redirects you to /<path>', fakeAsync(() => { 
+    router.navigate(['/stage']); 
+    tick(); 
+    expect(location.path()).toBe('/stage');
+
+    router.navigate(['/settings']); 
+    tick(); 
+    expect(location.path()).toBe('/settings');
+  }));
 
 });
