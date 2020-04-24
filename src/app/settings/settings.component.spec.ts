37916b69fdc70fa7a39237d1569790b7c64243dd
kl4532 <kornellukasiak@gmail.com>
2020-04-24 09:01:49 +0200

    tests added

--- src/app/settings/settings.component.spec.ts
+++ src/app/settings/settings.component.spec.ts
@@ -1,13 +1,28 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SettingsComponent } from './settings.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, By } from '@angular/platform-browser';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from '../app.component';
+import { DataService } from '../services/data.service';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
   let fixture: ComponentFixture<SettingsComponent>;
+  let srv: DataService;
+
+  let elements = {
+    btnGenerate: () => document.body.querySelector('#generate') as HTMLDivElement,
+    btnSubmit: () => document.body.querySelector('.btn-submit') as HTMLDivElement,
+  }
+
+  const mockResults: Object[] = [
+    {stage: "Test", length: 3.5, surface: "Tarmac", record: "02:23.88", country: "Japan"}, 
+    {stage: "Test II", length: 3.5, surface: "Tarmac", record: "02:25.77", country: "Japan"}
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,11 +34,17 @@ describe('SettingsComponent', () => {
         RouterTestingModule
       ],
       declarations: [ SettingsComponent ]
+      ,
+      providers: [
+          DataService
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    srv = TestBed.get(DataService);
+    spyOn(srv, 'filterStages').and.returnValue(of(mockResults))
     fixture = TestBed.createComponent(SettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -32,4 +53,22 @@ describe('SettingsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should generate tracks', () => {
+    elements.btnGenerate().click();
+    fixture.detectChanges();
+    expect(component.generated).toBeTruthy();
+  });
+
+  it('should submit and set settings', () => {
+    spyOn(srv, 'setSettings').and.callThrough();
+
+    elements.btnGenerate().click();
+    fixture.detectChanges();
+
+    elements.btnSubmit().click();
+    fixture.detectChanges();
+    
+    expect(srv.setSettings).toHaveBeenCalled();
+  })
 });
